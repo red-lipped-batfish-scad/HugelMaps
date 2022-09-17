a1447b6c4d9069ba8f56c3f8b0812316a3bf360a
AnthonyAl-Rifai <anthonyalrifai@gmail.com>
2022-09-17 10:22:57 -0500

    day-one-finished

---
+++ client/containers/PostContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PostCreator from '../components/PostCreator';
+
+const PostContainer = () => {
+    return (
+        null
+    );
+};
+
+export default PostContainer;
